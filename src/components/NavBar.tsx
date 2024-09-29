18a1e66fd3d0532b7d74a91bac0c26487514607f
Owen Rummage <owen@rummage.cc>
2024-09-29 13:40:36 -0500

    update navbar links, I dont know how I didnt notice them being broken for this long

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,12 +5,12 @@ export default function NavBar() {
   return (
     <div className="navbar sticky top-0 bg-black w-screen mb-24" id="Navbar">
         <div className="px-4 py-4 xl:px-24 flex flex-row" id="Navbar.Container">
-                <a href="/">
+                <a href="https://nixlabs.dev/">
                     <Image src={Logo} alt="NixLabs Logo" className="h-8 w-8 mr-auto" id="Navbar.Container.Logo"/>
                 </a>
                 <div id="Navbar.Container.LinkContainer" className="ml-auto flex flex-row items-center gap-4 text-zinc-400">
-                    <a href="/services/carrier" className=" text-md font-medium">Carrier Services</a>
-                    <a href="/services/hosting" className="text-md font-medium">Hosting Services</a>
+                    <a href="https://nixlabs.dev/services/carrier" className=" text-md font-medium">Carrier Services</a>
+                    <a href="https://nixlabs.dev/services/hosting" className="text-md font-medium">Hosting Services</a>
                 </div>
         </div>
     </div>
